dd552d100e8e04a08caf3a66586b7f65d38862c5
Susitha Ravinda <susitha.ravinda@cinglevue.com>
2017-05-14 21:52:35 +0530

    use resource server

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -15,7 +15,7 @@ angular.module('hello', ['ngRoute'])
     })
     .controller('home', function($http) {
         var self = this;
-        $http.get('/resource/').then(function(response){
+        $http.get('http://localhost:9000').then(function(response){
             self.greeting = response.data;
         });
         //this.greeting = {id: '186bec03-4f8f-40f9-948d-78a8140cd57a', content: 'Hello World!'}
